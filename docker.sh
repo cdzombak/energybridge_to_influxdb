bdf319e1fd97f2f35e48b39f2fe1b72b1f3ecd62
Andy Blyler <andy@blyler.cc>
2021-02-19 12:15:10 -0500

    Add docker support

---
+++ docker.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+warn () {
+    echo "$@" >&2
+}
+die () {
+    rc=$1
+    shift
+    warn "$@"
+    exit $rc
+}
+
+# check for required envs
+if [[ -z "${BRIDGE_HOST}" ]]; then
+    die 1 "BRIDGE_HOST is a required environment variables that is missing"
+fi
+
+if [[ -z "${BRIDGE_NAME_TAG}" ]]; then
+    die 1 "BRIDGE_NAME_TAG is a required environment variables that is missing"
+fi
+
+if [[ -z "${INFLUX_BUCKET}" ]]; then
+    die 1 "BRIDGE_NAME_TAG is a required environment variables that is missing"
+fi
+
+if [[ -z "${INFLUX_SERVER}" ]]; then
+    die 1 "BRIDGE_NAME_TAG is a required environment variables that is missing"
+fi
+
+# parse envs to create cmd args
+args=""
+
+if [[ $CLIENT_ID ]]; then
+    args="$args -client-id $CLIENT_ID"
+fi
+
+if [[ $BRIDGE_HOST ]]; then
+    args="$args -energy-bridge-host $BRIDGE_HOST"
+fi
+
+if [[ $BRIDGE_NAME_TAG ]]; then
+    args="$args -energy-bridge-nametag $BRIDGE_NAME_TAG"
+fi
+
+if [[ $INFLUX_BUCKET ]]; then
+    args="$args -influx-bucket $INFLUX_BUCKET"
+fi
+
+if [[ $INFLUX_SERVER ]]; then
+    args="$args -influx-server $INFLUX_SERVER"
+fi
+
+if [[ $INFLUX_USERNAME ]]; then
+    args="$args -influx-username $INFLUX_USERNAME"
+fi
+
+if [[ $INFLUX_PASSWORD ]]; then
+    args="$args -influx-password $INFLUX_PASSWORD"
+fi
+
+energybridge_to_influxdb $args
